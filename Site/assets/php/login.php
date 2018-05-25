a8961e133897135ebca3ba01d08b99b2fd419d78
Timothee Simon <xorob0@posteo.net>
2018-05-25 14:05:15 +0200

    Working login script using mysql

--- Site/assets/php/login.php
+++ Site/assets/php/login.php
@@ -1,29 +1,44 @@
 <?php
-   include("config.php");
-   session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$_POST['login']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['pwd']); 
-      
-      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-         
-         header("location: welcome.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
-      }
-   }
-?>
+date_default_timezone_set('Europe/Brussels');
+session_start();
+
+if(isset($_POST['login']) || isset($_POST['pwd']))
+{
+	if(!empty($_POST['login']) && !empty($_POST['pwd']))
+	{
+		// Defining variables
+		$login = htmlspecialchars($_POST['login']);
+		$pwd = sha1($_POST['pwd']);
+
+		# Connecting to the Mysql database
+
+		$sql = 'SELECT * FROM users WHERE login = ":login" && pwd = ":pwd" ';
+		$param = array('login' => $_POST['login'], 'pwd' => $pwd);
+		try
+		{
+			$bdd = new PDO('mysql:host=localhost;dbname=projet', 'root', 'Admin2015');
+		}
+		catch (Exception $e) 
+		{
+			echo '<p> Error connecting to the database, please try again<p> '.$e; //TODO redirect an error page and log error
+		}
+
+		$req = $bdd -> prepare($sql);
+
+		$data = $req -> execute($param);
+
+
+		if(!empty($data))
+		{ 
+			$_SESSION['connected'] = 1;
+			$_SESSION['id'] = $data['id'];
+			$_SESSION['login'] = $data['login'];
+			header('Location: ../../objects_list.html');
+			}
+		else
+		{
+			session_destroy();
+			echo '<p> Login or password invalid ! </p>';
+		}
+	}
+}
