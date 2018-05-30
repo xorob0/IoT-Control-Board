bd77477f2a0e9f021e01f38eb6ec691d205bb00a
Timothee Simon <xorob0@posteo.net>
2018-05-30 16:04:13 +0200

    Updated the organization

--- Site/settings.php
+++ Site/settings.php
@@ -40,7 +40,7 @@
             <div class="container d-flex hero" style="background-color:#ffffff;padding:0px;">
                 <ul class="list-group">
                     <li class="list-group-item"><span>Settings</span></li>
-                    <li class="list-group-item"><a href="addDevice.php">Add Device</a></li>
+                    <li class="list-group-item"><a href="add_device.php">Add Device</a></li>
                 </ul>
                 <div class="d-flex flex-column flex-fill" style="padding:10px;">
                     <h2 class="justify-content-center">Settings</h2>
