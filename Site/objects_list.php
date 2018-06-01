fc44d8d438570e446c77dc1deac7c9b8ba5fda60
Timothee Simon <xorob0@posteo.net>
2018-06-01 12:39:47 +0200

    Implemented the template file everywhere

--- Site/objects_list.php
+++ Site/objects_list.php
@@ -4,51 +4,8 @@
 	session_start();
 	require_once('assets/php/modele/show.php');
 	require_once('assets/php/modele/idCheck.php');
+	include('template.php');
 ?>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projet</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
-    <link rel="stylesheet" href="assets/css/Data-Table.css">
-    <link rel="stylesheet" href="assets/css/Data-Table.css">
-    <link rel="stylesheet" href="assets/css/Header-Dark.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
-    <link rel="stylesheet" href="assets/css/Rounded-Slider-with-Label.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <link rel="stylesheet" href="assets/css/Toggle-Switch.css">
-    <link rel="stylesheet" href="assets/css/Toggle-Switch.css">
-</head>
-
-<body>
-    <div>
-        <div class="header-dark">
-            <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
-				<div class="container"><a class="navbar-brand" href="#">IoT Control Board
-
-</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                    <div class="collapse navbar-collapse"
-                        id="navcol-1">
-                        <ul class="nav navbar-nav">
-                            <li class="nav-item" role="presentation"><a class="nav-link" href="settings.php">Settings</a></li>
-                            <li class="nav-item" role="presentation"><a class="nav-link" href="assets/php/logout.php">Log Out</a></li>
-                            <li class="dropdown"><a class="dropdown-toggle nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">Groups&nbsp;</a>
-                                <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
-                            </li>
-                        </ul>
-                        <form class="form-inline mr-auto" target="_self">
-                            <div class="form-group"><label for="search-field"></label></div>
-					</form><span class="navbar-text"><a href="settings.php">
-						<?php
-							echo $_SESSION['login'];
-						?>
-					</a></span></div>
-                </div>
-            </nav>
             <div class="container hero" style="background-color:#ffffff;padding:0px;">
                 <div class="table-responsive">
                     <table class="table">
